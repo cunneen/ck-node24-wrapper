022f572cc03ec8d4c36ef03a1bc6a693aa115f65
Mike Cunneen <mike@appworkshop.net>
2025-08-14 10:47:42 +0800

    docs: :memo: improve example

--- example/index.js
+++ example/index.js
@@ -8,6 +8,13 @@ const cert = new chilkat.Cert();
 // Load any type of certificate (.cer, .p7b, .pem, etc.) by calling LoadFromFile.
 const success = cert.LoadFromFile(path.join(__dirname, CERTFILE));
 if (success) {
+  console.log("SubjectDN:" + cert.SubjectDN);
+
+  console.log("Common Name:" + cert.SubjectCN);
+  console.log("Issuer Common Name:" + cert.IssuerCN);
+
+  console.log("Serial Number:" + cert.SerialNumber);
+
   console.log(cert.ExportCertPem());
 } else {
   console.error("failed to load cert");
